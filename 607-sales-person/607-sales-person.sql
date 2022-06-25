368e0227e345041b1502bcce664e00ca19ba3b81
Hesham Mohamed <heshamamohamed323@gmail.com>
2022-06-25 13:00:23 +0200

    Time: 1050 ms (87.71%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -4,11 +4,9 @@ FROM SalesPerson s
 WHERE s.sales_id NOT IN (
                             SELECT o.sales_id
                             FROM Orders o
-                            inner join Company C
-                            on O.com_id=C.com_id
                             WHERE o.com_id = (
-                                            SELECT t.com_id 
-                                            FROM company t 
-                                            WHERE t.name = 'RED' 
+                                            SELECT c.com_id 
+                                            FROM company c 
+                                            WHERE c.name = 'RED' 
                                             )
                         )
