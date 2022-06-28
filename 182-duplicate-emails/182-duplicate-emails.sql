57ed508cb52c2422768a9fd3a79f0b85df0b1c9e
Hesham Mohamed <heshamamohamed323@gmail.com>
2022-06-28 08:12:15 +0200

    Time: 338 ms (76.89%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 SELECT DISTINCT P2.Email
-FROM Person P1
-JOIN Person P2
-ON P1.id != P2.id
+FROM Person P1, Person P2
+WHERE P1.id != P2.id
 AND P1.email = P2.Email
