81f5e35608b2d38346f9922b2e29c947c08a8250
Hesham Mohamed <heshamamohamed323@gmail.com>
2022-07-10 02:56:20 +0200

    Time: 108 ms (73.47%), Space: 210.9 MB (82.24%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.rb
+++ 14-longest-common-prefix/14-longest-common-prefix.rb
@@ -1,12 +1,11 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
-
+  prefix = ''
   
   strs[0].each_char.with_index do |char, idx|
-    unless strs.all? { |string| string[idx] == char }
-        return strs[0][0...idx]
-    end
+    strs.all? { |string| string[idx] == char } ? prefix += char : break
   end
   
+  prefix
 end
