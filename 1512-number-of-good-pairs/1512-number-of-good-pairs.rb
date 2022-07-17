6265fb15b51504ee18e9402a031ee09c5cc0e815
Hesham Mohamed <heshamamohamed323@gmail.com>
2022-07-17 20:29:03 +0200

    Time: 100 ms (72.73%), Space: 211.1 MB (9.09%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.rb
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.rb
@@ -1,16 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def num_identical_pairs(nums)
-    # nums.combination(2).to_a.map {|pair| pair[0] == pair[1] ? 1 : 0 }.sum
-    
-    # nums.combination(2).to_a.count {|pair| pair[0] == pair[1] ? 1 : 0 }
-    
-    hash = Hash.new(0)
-    counter = 0
-    nums.each do |num|
-        counter += hash[num]
-        hash[num] += 1
-    end
-    
-    counter
+    nums.combination(2).count {|pair| pair[0] == pair[1]}
 end
