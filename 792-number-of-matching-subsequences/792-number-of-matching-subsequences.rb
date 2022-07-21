88e9da7b091d6c49a9a174ade0040434fc989620
Hesham Mohamed <heshamamohamed323@gmail.com>
2022-07-21 21:27:12 +0200

    Time: 1113 ms (100.00%), Space: 212 MB (100.00%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.rb
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.rb
@@ -2,16 +2,10 @@
 # @param {String[]} words
 # @return {Integer}
 def num_matching_subseq(s, words)
-  # string_hash = Hash.new() {|k,v| k[v] = [] }
-  # s.chars.zip(0...s.size).each { |char, index| string_hash[char] << index }
-  
-  # byebug
   words.count do |word|
     new_s = s
     word.chars.all? do |char|
-      if new_s.include?(char)
-        new_s = new_s[new_s.index(char)+1..-1]
-      end
+      new_s = new_s[new_s.index(char)+1..-1] if new_s.include?(char)
     end
   end
 end
