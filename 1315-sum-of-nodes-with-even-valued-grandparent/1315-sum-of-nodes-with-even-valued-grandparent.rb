47758f18d4427013eaa5de2a7a65d3589e03ef94
Hesham Mohamed <heshamamohamed323@gmail.com>
2022-07-22 09:36:55 +0200

    Time: 178 ms (93.75%), Space: 213.8 MB (12.50%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.rb
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def sum_even_grandparent(root)
+    sum = []
+    get_sum(root, sum)
+    sum.sum
+end
+
+def get_sum(node, sum, parent = nil, grandparent = nil)
+    return if node.nil?
+    sum << node.val if grandparent&.even?
+    
+    get_sum(node.left, sum, node.val, parent)
+    get_sum(node.right, sum, node.val, parent)
+end
